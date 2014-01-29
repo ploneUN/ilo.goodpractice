2694ec97bb42ec615eb06dd73a6d55f338293b35
Khairul Anwar (kulll) <anwarbaik88@gmail.com>
2014-01-29 16:46:19 +0800

    add initial fields (re #1031)

--- ilo/goodpractice/content/ilo_good_practice_form.py
+++ ilo/goodpractice/content/ilo_good_practice_form.py
@@ -29,8 +29,67 @@ from ilo.goodpractice import MessageFactory as _
 
 class IILOGoodPracticeForm(form.Schema, IImageScaleTraversable):
     """
-    
+    Good Practice Form
     """
-    pass
+    title = schema.TextLine(
+        title=_(u'Evaluation Title'),
+        description=u'',
+        required=True,
+        )
+
+    description = schema.TextLine(
+        title=_(u'Evaluation Title'),
+        description=u'',
+        required=True,
+        )
+
+    brief_summary = RichText(
+        title=_(u'Brief summary of the good practice'),
+        description=_(u'Brief summary of the good practice (link to'
+                      u' project goal or specific deliverable, background,'
+                      u' purpose, etc.'),
+        required=True,
+        )
+
+    relevant_condition = RichText(
+        title=_(u'Relevant conditions and Context'),
+        description=_(u'Relevant conditions and Context: limitations or advice'
+                      u' in terms of applicability  and replicability'),
+        required=True,
+        )
+
+    cause_effect = RichText(
+        title=_(u'Cause-effect relationship'),
+        description=_(u'Establish a clear cause-effect relationship'),
+        required=True,
+        )
+
+    impact = RichText(
+        title=_(u'Measurable impact'),
+        description=_(u'Indicate measurable impact and targeted'
+                      u' beneficiaries'),
+        required=True,
+        )
+
+    replication = RichText(
+        title=_(u'Potential for replication'),
+        description=_(u'Potential for replication and by whom'),
+        required=True,
+        )
+
+    upward_link = RichText(
+        title=_(u'Upward links to higher ILO Goals'),
+        description=_(u'Upward links to higher ILO Goals (DWCPs,'
+                      u" Country Programme Outcomes or ILO's Strategic"
+                      u' Programme Framework)'),
+        required=True,
+        )
+
+    other = RichText(
+        title=_(u'Other documents'),
+        description=_(u'Other documents or relevant comments'),
+        required=True,
+        )
+
 
 alsoProvides(IILOGoodPracticeForm, IFormFieldProvider)
