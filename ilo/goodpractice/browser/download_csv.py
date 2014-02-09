947768b662b9c85cc942c9cc3f9498085041fb47
Khairil Yusof <khairil.yusof@gmail.com>
2014-02-09 16:09:53 +0800

    Use UnicodeWriter to make sure csv export works with unicode character. Partial commit, needs a bit more to finish it up including adding EVAL themes.

--- ilo/goodpractice/browser/download_csv.py
+++ ilo/goodpractice/browser/download_csv.py
@@ -1,7 +1,8 @@
 from five import grok
 from Products.CMFCore.interfaces import IContentish
-import csv
-from cStringIO import StringIO
+from Products.CMFPlone.utils import safe_unicode
+import csv, codecs, cStringIO
+
 
 class DownloadCSV(grok.View):
     grok.context(IContentish)
@@ -10,12 +11,10 @@ class DownloadCSV(grok.View):
 
     def render(self):
 
-
-        out = StringIO()
-        writer = csv.writer(out)
+        
        
         #Column Headings
-        headings = ['Good Practice',
+        headings = [  'Good Practice',
                       'Creator',
                       'Start Date',
                       'End Date',
@@ -28,8 +27,6 @@ class DownloadCSV(grok.View):
                       'Keywords',
                       'Comments',
                     ]
-
-        writer.writerow(headings)
         
         #Rows
         brains = self.context.results()
@@ -39,8 +36,8 @@ class DownloadCSV(grok.View):
             row = []
             row.append(obj.title)
             row.append(obj.Creator()) #XXX need to covert to fullname
-            row.append(obj.start)
-            row.append(obj.end)
+            row.append(obj.start.isoformat()) #
+            row.append(obj.end.isoformat()) ###XXX needs to be formatted as string
             row.append(obj.description)
             row.append(obj.goodpractice_context)
             row.append(obj.goodpractice_cause_effect)
@@ -49,10 +46,34 @@ class DownloadCSV(grok.View):
             row.append(obj.goodpractice_link)
             row.append(obj.goodpractice_keywords)
             row.append(obj.goodpractice_comment)
-            writer.writerow(row)
-    
+        
+
+        out = cStringIO.StringIO()
+
+        import ipdb; ipdb.set_trace()
+
+        writer = UnicodeWriter(self, out)  
+
         filename = "good_practice.csv"
         self.request.response.setHeader('Content-Type', 'text/csv')
         self.request.response.setHeader('Content-Disposition', 'attachment; filename="%s"' % filename)
 
         return out.getvalue()
+
+class UnicodeWriter:
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8-sig", **kwds):
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        data = self.encoder.encode(data)
+        self.stream.write(data)
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
